38611c58a9560b29c6833b0307bb37e1c4709185
torni <tornanar@gmail.com>
2025-10-10 23:30:59 +0300

    lesson11 homework

--- src/main/kotlin/ru/stimmax/lessons/lesson10/homeworks/homeworks.kt
+++ src/main/kotlin/ru/stimmax/lessons/lesson10/homeworks/homeworks.kt
@@ -21,10 +21,12 @@ fun main() {
     println(mmap3)
     //7. Создайте словарь (ключи Double, значения Int)
 // и выведи в цикле результат деления ключа на значение. Не забудь обработать деление на 0 (в этом случае выведи слово “бесконечность”)
-    val  mmap4: Map<Double, Int> = mapOf(1.0 to 3, 2.0 to 1, 3.0 to 0, 4.0 to 25)
-    for ((k,v) in mmap4){
-        if (v==0){println("Бесконечность"); continue}
-        println(k/v)
+    val mmap4: Map<Double, Int> = mapOf(1.0 to 3, 2.0 to 1, 3.0 to 0, 4.0 to 25)
+    for ((k, v) in mmap4) {
+        if (v == 0) {
+            println("Бесконечность"); continue
+        }
+        println(k / v)
     }
     //8. Измените значение для существующего ключа в изменяемом словаре.
     val mmap5: MutableMap<Int, String> = mutableMapOf<Int, String>(1 to "Адын")
@@ -36,38 +38,39 @@ fun main() {
     val map6: Map<Int, String> = mapOf(1 to "1", 2 to "2", 3 to "3")
     val map7: Map<Int, String> = mapOf(4 to "4", 5 to "5", 6 to "6")
     val mmap8: MutableMap<Int, String> = mutableMapOf()
-    for ((k,v) in map6){
+    for ((k, v) in map6) {
         mmap8[k] = v
     }
-    for ((k,v) in map7){
+    for ((k, v) in map7) {
         mmap8[k] = v
     }
     println(mmap8)
 
     //10. Создайте словарь, где ключами являются строки, а значениями - списки целых чисел. Добавьте несколько элементов в этот словарь.
     val mmap9 = mutableMapOf<String, List<Int>>()
-    mmap9["Важные дни"] = listOf<Int>(1,2,3,5)
-    mmap9["Неважные дни"] = listOf<Int>(4,6,7)
+    mmap9["Важные дни"] = listOf<Int>(1, 2, 3, 5)
+    mmap9["Неважные дни"] = listOf<Int>(4, 6, 7)
     println(mmap9)
 
-        //11. Создай словарь, в котором ключи - это целые числа, а значения - изменяемые множества строк. Добавь данные в словарь.
+    //11. Создай словарь, в котором ключи - это целые числа, а значения - изменяемые множества строк. Добавь данные в словарь.
 // Получи значение по ключу (это должно быть множество строк) и добавь в это множество ещё строку. Распечатай полученное множество.
-    val map10 = mutableMapOf<Int, MutableSet<String>>(1 to mutableSetOf("Hello","Kotlin"))
+    val map10 = mutableMapOf<Int, MutableSet<String>>(1 to mutableSetOf("Hello", "Kotlin"))
     map10[1]?.add("again")
     println(map10)
     //12. Создай словарь, где ключами будут пары чисел. Через перебор найди значение
 // у которого пара будет содержать цифру 5 в качестве первого или второго значения.
-    val map11 :Map<Pair<Int, Int>, String> = mapOf((1 to 1)to "Куку",(5 to 1) to "Попався!")
-    for ((k,v) in map11){
-        if (k.first == 5 || k.second==5){
+    val map11: Map<Pair<Int, Int>, String> = mapOf((1 to 1) to "Куку", (5 to 1) to "Попався!", (2 to 5) to "Повався")
+    for ((k, v) in map11) {
+        if (k.first == 5 || k.second == 5) {
             println(map11[k])
         }
     }
     //Задачи на подбор оптимального типа для словаря
     //1. Словарь библиотека: Ключи - автор книги, значения - список книг
-    val a: MutableMap<String, MutableList<String>> = mutableMapOf("Терри Пратчет" to mutableListOf("Цвет волшебства","Последний континент"))
+    val a: MutableMap<String, MutableList<String>> =
+        mutableMapOf("Терри Пратчет" to mutableListOf("Цвет волшебства", "Последний континент"))
     //2. Справочник растений: Ключи - типы растений (например, "Цветы", "Деревья"), значения - списки названий растений
-    val  a2: MutableMap<String, MutableList<String>> = mutableMapOf<String, MutableList<String>>()
+    val a2: MutableMap<String, MutableList<String>> = mutableMapOf<String, MutableList<String>>()
     //3. Четвертьфинала: Ключи - названия спортивных команд, значения - списки игроков каждой команды
     val a3 = mapOf<String, MutableList<String>>()
     //4. Курс лечения: Ключи - даты, значения - список препаратов принимаемых в дату
---
+++ src/main/kotlin/ru/stimmax/lessons/lesson11/homeworks/homeworks11.kt
@@ -0,0 +1,101 @@
+package ru.stimmax.lessons.lesson11.homeworks
+
+fun main() {
+    println(multiplyByTwo(2))
+    println(isEven(2))
+    println(isEven(0))
+    println(isEven(1))
+    printNumbersUntil(14)
+    println()
+    println(findFirstNegative(listOf(1, 2, 3, -5, 0, 2, 3, 5, -100)))
+    processList(listOf("Hello", "World", null,"null"))
+}
+
+
+// Задачи на сигнатуру метода
+//1. Не принимает аргументов и не возвращает значения.
+fun a1() {}
+
+//2. Принимает два целых числа и возвращает их сумму.
+fun a2(a: Int, b: Int): Int = a + b
+
+//3. Принимает строку и ничего не возвращает.
+fun a3(s: String) {}
+
+//4. Принимает список целых чисел и возвращает среднее значение типа Double.
+fun a4(l: List<Int>): Double {
+    return 0.0
+}
+
+//5. Принимает nullable строку и возвращает её длину в виде nullable целого числа и доступна только в текущем файле.
+private fun a5(s: String?): Int? {
+    return 2
+}
+
+//6. Не принимает аргументов и возвращает nullable вещественное число.
+fun a6(): Double? {
+    return 0.0
+}
+
+//7. Принимает nullable список целых чисел, не возвращает значения и доступна только в текущем файле.
+private fun a8(l: List<Int>?) {}
+
+//8. Принимает целое число и возвращает nullable строку.
+fun a8(i: Int): String? {
+    return ""
+}
+
+//9. Не принимает аргументов и возвращает список nullable строк.
+fun a9(): List<String>? {
+    return null
+}
+
+//10. Принимает nullable строку и nullable целое число и возвращает nullable булево значение.
+fun a10(s: String?, i: Int?): Boolean? {
+    return true
+}
+
+//Задачи на написание кода
+//11. Напишите функцию multiplyByTwo, которая принимает целое число и возвращает его, умноженное на 2.
+fun multiplyByTwo(i: Int): Int = i * 2
+
+//12. Создайте функцию isEven, которая принимает целое число и возвращает true, если число чётное, и false в противном случае.
+fun isEven(i: Int): Boolean {
+    return if (i % 2 == 0) true else false
+}
+
+//13. Напишите функцию printNumbersUntil, которая принимает целое число n и выводит на экран числа от 1 до n.
+// Если число n меньше 1, функция должна прекратить выполнение с помощью return без вывода сообщений.
+fun printNumbersUntil(n: Int) {
+    if (n < 1) {
+        return
+    } else {
+        for (x in 1 until n) {
+            print("$x ")
+        }
+    }
+}
+
+//14. Создайте функцию findFirstNegative, которая принимает список целых чисел и возвращает первое отрицательное число в списке.
+// Если отрицательных чисел нет, функция должна вернуть null.
+fun findFirstNegative(il: List<Int>): Int? {
+    var res: Int? = null
+    for (x in il) {
+        if (x < 0) {
+            res = x
+            break
+        }
+    }
+    return res
+}
+
+//15. Напишите функцию processList, которая принимает список строк. Функция должна проходить по списку и выводить каждую строку.
+// Если встречается null значение, функция должна прекратить выполнение с помощью return без возврата значения.
+fun processList(sl: List<String?>) {
+    for (x in sl) {
+        if (x == null) {
+            return
+        } else println(x)
+    }
+
+}
