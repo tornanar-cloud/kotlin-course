ee34177652616f3ed8526ccb2a6b3d3bfa28b265
torni <tornanar@gmail.com>
2025-11-06 01:33:45 +0300

    lesson 17 homework

--- src/main/kotlin/ru/stimmax/lessons/lesson17/ArcadeMachine.kt
+++ src/main/kotlin/ru/stimmax/lessons/lesson17/ArcadeMachine.kt
@@ -3,13 +3,13 @@ package ru.stimmax.lessons.lesson17
 class ArcadeMachine(val name: String, val model: String, private var pin: String = "1111") {
     private var isOn = false
     private var isOsLoaded = false
-    private var installedGame = mutableListOf<String>("CS2", "Dota 2", "Hots")
+    private var installedGames = mutableListOf<String>("CS2", "Dota 2", "Hots")
     private var hasJoystic = true
     private var balance = 0.0
     private var supportPhoneNumber = "+7777777777"
     private var isSessionPaid = false
     private var gameSessionPrice = 20.0
-    private var owner: String =""
+    private var owner: String = ""
 
     fun turnOn() {
         if (isOn) {
@@ -21,13 +21,63 @@ class ArcadeMachine(val name: String, val model: String, private var pin: String
         }
     }
 
-    fun turnOff(){}
+    fun turnOff() {
+        unloadOs()
+        isOn = false
+        println("Игровой автомат выключен")
+    }
 
 
-    private fun loadOs(){
+    private fun loadOs() {
         isOsLoaded = true
         println("Загрузил ОС")
     }
 
-    private fun
+    private fun unloadOs() {
+        isOsLoaded = false
+        println("Выгрузил ОС")
+    }
+
+    fun showInstalledGames() {
+        println(installedGames)
+    }
+
+    fun startGame(game: String) {
+        if (isSessionPaid) {
+            println("Запустил игру.\n Играем в игру $game\nЗакончили играть")
+            isSessionPaid = false
+        } else {
+            println("Нужно оплатить игровую сессию")
+        }
+    }
+
+    fun payGameSession() {
+        if (balance >= gameSessionPrice) {
+            balance -= gameSessionPrice
+            isSessionPaid = true
+            println("Игровая сессия оплачена")
+        } else {
+            println("Недостаточно средств на балансе")
+        }
+    }
+
+    fun addToBalance(amount: Double) {
+        balance += Math.abs(amount)
+        println("Пополнение прошло успешно. Текущий баланс $balance")
+    }
+
+    fun withdrawCash(pin: String): Double {
+        return if (this.pin == pin) {
+            openSafeAndDispenseCash()
+        } else {
+            println("Неверный пин")
+            0.0
+        }
+    }
+
+    private fun openSafeAndDispenseCash(): Double {
+        val a = balance
+        balance = 0.0
+        return a
+    }
 }
