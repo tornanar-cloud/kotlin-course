b4e9780f14d937cdb912472316ee2950749ab56a
torni <tornanar@gmail.com>
2025-12-15 01:53:50 +0300

    lesson 27 homework

--- src/main/kotlin/ru/stimmax/lessons/lesson27/homeworks/Homework27.kt
+++ src/main/kotlin/ru/stimmax/lessons/lesson27/homeworks/Homework27.kt
@@ -1,7 +1,6 @@
 package ru.stimmax.lessons.lesson27.homeworks
 
 
-
 /*
 Задание 1
 Создай Enum со статусами прохождения теста (passed, broken, failed).
@@ -30,52 +29,54 @@ enum class ResidentialPropertyType(val type: String) {
     companion object {
         fun printResidentialPropertyTypeInfoBySortEnumName() {
             val b = entries
-            b.map { mapOf(it.name to it.type).entries.first() }.sortedBy { it.key.length }.forEach { println("Enum - ${it.key} | Тип - ${it.value}") }
+            b.map { mapOf(it.name to it.type).entries.first() }.sortedBy { it.key.length }
+                .forEach { println("Enum - ${it.key} | Тип - ${it.value}") }
         }
     }
-
-    enum class Planet(
-        val distanceFromSunAu: Double,
-        val massKg: Double
-    ) {
-        MERCURY(
-            distanceFromSunAu = 0.39,
-            massKg = 3.3011e23
-        ),
-        VENUS(
-            distanceFromSunAu = 0.72,
-            massKg = 4.8675e24
-        ),
-        EARTH(
-            distanceFromSunAu = 1.0,
-            massKg = 5.97237e24
-        ),
-        MARS(
-            distanceFromSunAu = 1.52,
-            massKg = 6.4171e23
-        ),
-        JUPITER(
-            distanceFromSunAu = 5.20,
-            massKg = 1.8982e27
-        ),
-        SATURN(
-            distanceFromSunAu = 9.58,
-            massKg = 5.6834e26
-        ),
-        URANUS(
-            distanceFromSunAu = 19.20,
-            massKg = 8.6810e25
-        ),
-        NEPTUNE(
-            distanceFromSunAu = 30.05,
-            massKg = 1.02413e26
-        )
-        ;
-    }
 }
+
+
+enum class Planet(
+    val distanceFromSunAu: Double,
+    val massKg: Double
+) {
+    MERCURY(
+        distanceFromSunAu = 0.39,
+        massKg = 3.3011e23
+    ),
+    VENUS(
+        distanceFromSunAu = 0.72,
+        massKg = 4.8675e24
+    ),
+    EARTH(
+        distanceFromSunAu = 1.0,
+        massKg = 5.97237e24
+    ),
+    MARS(
+        distanceFromSunAu = 1.52,
+        massKg = 6.4171e23
+    ),
+    JUPITER(
+        distanceFromSunAu = 5.20,
+        massKg = 1.8982e27
+    ),
+    SATURN(
+        distanceFromSunAu = 9.58,
+        massKg = 5.6834e26
+    ),
+    URANUS(
+        distanceFromSunAu = 19.20,
+        massKg = 8.6810e25
+    ),
+    NEPTUNE(
+        distanceFromSunAu = 30.05,
+        massKg = 1.02413e26
+    )
+    ;
+}
+
 //Задание 2
 //Создай функцию, которая выводит на печать человекочитаемые названия типов недвижимости в порядке возрастания длины названия enum.
-
 
 
 /*
@@ -88,19 +89,29 @@ Enum со статусом прохождения теста в зависимо
 прочие исключения  - BROKEN
  */
 
-fun  fn3(func:()->Unit): TestStatus{
+fun fn3(func: () -> Unit): TestStatus {
     return try {
         func()
         TestStatus.passed
-    }catch (e: AssertionError) {
+    } catch (e: AssertionError) {
         println(e)
         return TestStatus.failed
-    }catch (e: Exception) {
+    } catch (e: Exception) {
         println(e)
         return TestStatus.broken
     }
 }
 
+
+/*
+Задание 4
+Создай функцию, которая принимает лямбду и возвращает enum планеты. Лямбда должна принимать планету и возвращать булево значение.
+Лямбда здесь выступает в качестве фильтра, который должен отфильтровать список всех планет по какому-либо признаку (расстояние или вес).
+Вернуть нужно первый элемент из отфильтрованного списка или выкинуть исключение если список пустой.
+ */
+fun fn4(p:(Planet) -> Boolean): Planet{
+    return Planet
+}
 
 fun main() {
     ResidentialPropertyType.printResidentialPropertyTypeInfoBySortEnumName()
